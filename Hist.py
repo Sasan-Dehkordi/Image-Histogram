be7a7f14e84d5e3adaa756d63c4d0a8fcc44eefb
Sasan-Dehkordi <62342910+Sasan-Dehkordi@users.noreply.github.com>
2021-01-02 09:04:46 +0100

    Create Hist.py

---
+++ Hist.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+im= Image.open('image.png')
+b, g ,r = im.split()
+GreenHist= g.histogram()
+BlueHist= b.histogram()
+RedHist= r.histogram()
+plt.plot(GreenHist, 'g')
+plt.plot(RedHist,'r')
+plt.plot(BlueHist, 'b')
+plt.xlabel('Pixel Value')
+plt.ylable('Frequency of Pixels')
